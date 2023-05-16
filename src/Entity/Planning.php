d61dd88ed2753dbc0b5ce189b83a15dbad6bd724
Anmol Vishvas <anmol.vishvas@sydev.com>
2023-05-16 14:59:45 +0400

    add collaborator in planning

--- src/Entity/Planning.php
+++ src/Entity/Planning.php
@@ -3,6 +3,11 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\GetCollection;
 use App\Repository\PlanningRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\Collection;
@@ -10,24 +15,47 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PlanningRepository::class)]
-#[ApiResource(denormalizationContext: ['groups' => ['planning:create']])]
+#[ApiResource(
+    denormalizationContext: ['groups' => ['planning_create']],
+    normalizationContext: ['groups' => ['read:Planning']],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Post(
+            uriTemplate: '/planning/{id}/add_collaborators',
+            requirements: ['id' => '\d+'],
+            status: 200,
+            denormalizationContext: ['groups' => ['planning_createCollaborator']]
+        ),
+        new Patch(),
+        new Post(
+            uriTemplate: '/planning/{id}/remove_collaborators',
+            requirements: ['id' => '\d+'],
+            status: 200
+        ),
+        new Delete(),
+    ]
+)]
 class Planning
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['read:Collaborator'])] 
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups('planning:create')]
+    #[Groups(['planning_create', 'read:Planning', 'read:Collaborator'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups('planning:create')]
+    #[Groups(['planning_create', 'read:Planning', 'read:Collaborator'])]
     private ?string $description = null;
 
 
     #[ORM\OneToMany(mappedBy: 'planning', targetEntity: Collaborator::class)]
+    #[Groups(['planning_createCollaborator', 'read:Planning'])]
     private Collection $collaborators;
 
     public function __construct() {
@@ -71,9 +99,9 @@ class Planning
         return $this->collaborators;
     }
 
-    public function addCollaborators(Collaborator $collaborators): self
+    public function addCollaborator(Collaborator $collaborator): self
     {
-        if (!$this->collaborators->contains($collaborators)) {
+        if (!$this->collaborators->contains($collaborator)) {
             $this->collaborators->add($collaborator);
             $collaborator->setPlanning($this);
         }
@@ -81,7 +109,7 @@ class Planning
         return $this;
     }
 
-    public function removeCollaborators(Collaborator $collaborator): self
+    public function removeCollaborator(Collaborator $collaborator): self
     {
         if ($this->collaborators->removeElement($collaborator)) {
             // set the owning side to null (unless already changed)
