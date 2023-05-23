5c156152ef3da6b417023b77bda642c22b30c8df
Anmol Vishvas <anmol.vishvas@sydev.com>
2023-05-23 16:11:49 +0400

    Comments removed in config folder

--- migrations/Version20230517082301.php
+++ migrations/Version20230517082301.php
@@ -7,25 +7,15 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230517082301 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `leave` CHANGE reason reason VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `leave` CHANGE reason reason LONGTEXT NOT NULL');
     }
 }
--- migrations/Version20230522065010.php
+++ migrations/Version20230522065010.php
@@ -7,19 +7,10 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20230522065010 extends AbstractMigration
 {
-    public function getDescription(): string
-    {
-        return '';
-    }
-
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `leave` ADD planning_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE `leave` ADD CONSTRAINT FK_9BB080D03D865311 FOREIGN KEY (planning_id) REFERENCES planning (id)');
         $this->addSql('CREATE INDEX IDX_9BB080D03D865311 ON `leave` (planning_id)');
@@ -27,7 +18,6 @@ final class Version20230522065010 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE `leave` DROP FOREIGN KEY FK_9BB080D03D865311');
         $this->addSql('DROP INDEX IDX_9BB080D03D865311 ON `leave`');
         $this->addSql('ALTER TABLE `leave` DROP planning_id');
