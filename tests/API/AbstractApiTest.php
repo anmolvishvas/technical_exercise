86ab88e2cf9aa753b5bdc9d510bdbfb5ba250b63
Anmol Vishvas <anmol.vishvas@sydev.com>
2023-05-25 17:08:13 +0400

    AbstractApiTest.php file

---
+++ tests/API/AbstractApiTest.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\API;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+abstract class AbstractApiTest extends ApiTestCase
+{
+    protected function loginUser(string $username, string $password):?string 
+    {
+        $client=static::createClient();
+
+        $response = $client->request('POST', '/api/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => json_encode(
+                [
+                    "username"=> $username,
+                    "password"=> $password
+                ]
+            ),
+        ]);
+        
+        return !empty($response->toArray()['token']) ?$response->toArray()['token'] :null;
+    }
+}
