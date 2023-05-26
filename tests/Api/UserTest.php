e651b516d69142e76432021774c7c41ba56cc04a
Anmol Vishvas <anmol.vishvas@sydev.com>
2023-05-26 09:01:11 +0400

    folder name changed

---
+++ tests/Api/UserTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Api;
+
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
+class UserTest extends ApiTestCase 
+{
+    public function testLoginWithExistingCredentials(): void
+    {
+        $client=static::createClient();
+
+        $client->request('POST', '/api/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => json_encode(
+                [
+                    "username"=> "1684483706_Anmol",
+                    "password"=> "Anmol"
+                ]
+            ),
+        ]);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLoginWithoutCredentials(): void
+    {
+        $client=static::createClient();
+
+        $client->request('POST', '/api/login', [
+            'headers' => ['Content-Type' => 'application/json']
+        ],);
+
+        $this->assertResponseStatusCodeSame(400);
+    }
+
+    public function testLoginWithNotExistingCredentials(): void
+    {
+        $client=static::createClient();
+
+        $client->request('POST', '/api/login', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'body' => json_encode(
+                [
+                    "username"=> "1684483706_Anmol2",
+                    "password"=> "Anmol2"
+                ]
+            ),
+        ],);
+
+        $this->assertResponseStatusCodeSame(401);
+    }
+}
